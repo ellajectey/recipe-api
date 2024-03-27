fb1899c38c854fcade9320674938ef645927b027
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-03-27 15:41:47 +0000

    additions

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { addRecipe, deleteRecipe, getRecipe, getRecipes, updateRecipe } from "../controllers/recipes.controller.js";
 import multer from "multer";
+import { MulterSaveFilesOrgStorage } from "multer-savefilesorg-storage";
 
 // configure (multer) upload middleware
-const upload = multer({dest:'uploads/images'});
-
+// const upload = multer({dest:'uploads/images'});
+const upload = multer({
+    storage:MulterSaveFilesOrgStorage({
+        serverPath: `https://savefiles.org/api/v1/uploads`,
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: '/recipes/images'
+    })
+});
 
 // create recipes router
 const router = Router();
